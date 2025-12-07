2ae49d16f5faa9cc49efd93cd8d979392dbeef7e
Rizky Warna <rizky.warna@gmail.com>
2025-12-08 01:36:50 +0700

    commitke3

--- src/app/daftar/page.jsx
+++ src/app/daftar/page.jsx
@@ -1,9 +1,13 @@
+import Kotak from '@/feature/daftar/container/Kotak'
 import Navbar from '@/shared/components/Navbar'
 import React from 'react'
 
 const page = () => {
   return (
+    <>
     <Navbar></Navbar>
+    <Kotak></Kotak>
+    </>
   )
 }
 
--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,14 +1,13 @@
-import Footer from '@/shared/components/Footer'
-import Navbar from   '@/shared/components/Navbar'
+import Navbar from '@/shared/components/Navbar'
+import Kotak from '@/feature/daftar/container/Kotak'
 import React from 'react'
 
 const page = () => {
   return (
     <>
-    <Navbar/>
-    <Footer/>
+      <Navbar />
+      <Kotak />
     </>
-    
   )
 }
 
