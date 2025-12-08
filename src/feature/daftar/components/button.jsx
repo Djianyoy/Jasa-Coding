e67b656627469c32b35acdae22a8e1256c5728d8
Rizky Warna <rizky.warna@gmail.com>
2025-12-08 21:44:50 +0700

    commitrizky

--- src/feature/daftar/components/button.jsx
+++ src/feature/daftar/components/button.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Button = () => {
   return (
     <>
-      <button className="w-[246px] h-[54px] rounded-3xl bg-[#868585] text-[#E4E1E1] text-xl font-semibold mx-auto block">
+      <button className="w-[246px] h-[54px] rounded-3xl bg-[#2E4C77] text-[#E4E1E1] text-xl font-semibold mx-auto block">
         <a href="/home">Daftar</a>
       </button>
 
-      <p className="text-center text-sm mt-4">
+      <p className="text-center text-sm mt-4 text-white">
         Sudah punya akun?{" "}
         <a href="/login" className="font-semibold hover:underline">Masuk</a>
       </p>
--- src/feature/daftar/container/Kotak.jsx
+++ src/feature/daftar/container/Kotak.jsx
@@ -8,14 +8,14 @@ const Kotak = () => {
       <div className="flex items-center">
         
         {/* KIRI */}
-        <div className="bg-[#6C6A6A] text-white rounded-3xl p-10 w-[470px] h-[586px] flex flex-col">
-          <h2 className="text-5xl font-bold mb-2">Selamat datang!</h2>
-          <p className="mb-10 text-lg">open joki bg</p>
-          <div className="bg-[#D4D4D4] w-[233px] h-[233px] mx-auto mt-12"></div>
-        </div>
+          <div className="bg-[#3D4450] text-white rounded-3xl p-10 w-[470px] h-[586px] flex flex-col">
+            <h2 className="text-5xl font-bold mb-2">Gabung dulu yuk!</h2>
+            <p className="mb-10 text-lg">Mulai akses layanan coding yang kamu butuhkan.</p>
+            <img src="/bgImage3.webp" alt="logo" className=" mx-auto mt-28" />
+          </div>
 
-        {/* KANAN */}
-        <div className="bg-[#C7C7C7BF] rounded-r-3xl p-22 w-[600px] min-h-[400px] flex flex-col justify-center">
+          {/* KANAN */}
+        <div className="bg-[#E09F24] rounded-r-3xl p-22 w-[600px] min-h-[400px] flex flex-col justify-center">
           <IsiInput />
           <Button />
         </div>
