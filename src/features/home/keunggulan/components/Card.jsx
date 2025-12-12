19bcb208a7629bd2294ab542598117349098fddb
Djianyoy <alifzildjian06@gmail.com>
2025-12-12 08:26:48 +0700

    kommit jian benerin dikit

--- src/features/home/keunggulan/components/Card.jsx
+++ src/features/home/keunggulan/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = ({ number, title, description }) => {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-xl w-full md:w-[250px] flex flex-col items-center gap-4">
+    <div className="overflow-x-hidden bg-white p-6 rounded-lg shadow-xl w-full md:w-[250px] flex flex-col items-center gap-4">
       <div className="bg-[#F2B33D] w-12 h-12 rounded-full flex items-center justify-center text-white font-bold text-xl">
         {number}
       </div>
